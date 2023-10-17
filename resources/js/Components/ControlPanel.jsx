a57c14ad21e5716a26c85f5e9b99dcc9dd300627
soewaiyanag <soewaiyanag@gmail.com>
2023-10-17 10:57:31 +0700

    add query route in href to all 3 filter Link buttons

--- resources/js/Components/ControlPanel.jsx
+++ resources/js/Components/ControlPanel.jsx
@@ -1,13 +1,40 @@
+import { Link, usePage } from "@inertiajs/react";
+
 export default function ControlPanel() {
+    const { props } = usePage();
+
+    const filterOptions = [
+        { label: "All", value: "" },
+        { label: "Active", value: "active" },
+        { label: "Completed", value: "completed" },
+    ];
+
     return (
         <div className="control-panel">
-            <span>5 items left</span>
+            <span>
+                {props.todos.filter((todo) => !todo.completed).length} items
+                left
+            </span>
             <div className="filters">
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                {filterOptions.map(({ label, value }) => (
+                    <Link
+                        as="button"
+                        key={label.toLowerCase()}
+                        href={value === "" ? "/" : `/?filter=${value}`}
+                        preserveScroll
+                    >
+                        {label}
+                    </Link>
+                ))}
             </div>
-            <button>Clear Completed</button>
+            <Link
+                as="button"
+                method="delete"
+                // href={route("todos.clear-completed")}
+                className="clear-completed"
+            >
+                Clear Completed
+            </Link>
         </div>
     );
 }
